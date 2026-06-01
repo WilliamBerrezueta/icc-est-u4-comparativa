c36907a77c1020b0867e722808a07d13f17df76e
WilliamBerrezueta <lolkio079@gmail.com>
2026-06-01 08:45:13 -0500

    second

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import models.Persona;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static Persona[] generarPersonas(int cantidad) {
+            Persona[] personas = new Persona[cantidad];
+            
+            for (int i = 0; i < cantidad; i++) {
+                String nombre = "Persona" + (i + 1);
+                int edad = (int) (Math.random() * 101);
+                personas[i] = new Persona(nombre, edad);
+            }
+            return personas;
+    }
+    public static void main(String[] args) throws Exception { 
+            
     }
 }
---
+++ src/models/Resultado.java
@@ -0,0 +1,65 @@
+package models;
+
+public class Resultado {
+
+    private String algoritmo;
+    private String escenario;
+    private int sample;
+    private double tiempoNano;
+    private double tiempoMilis;
+    
+    public Resultado(String algoritmo, String escenario, int sample, double tiempoNano) {
+        this.algoritmo = algoritmo;
+        this.escenario = escenario;
+        this.sample = sample;
+        this.tiempoNano = tiempoNano;
+        this.tiempoMilis = tiempoNano / 100000.0;
+    }
+
+    public String getAlgoritmo() {
+        return algoritmo;
+    }
+
+    public void setAlgoritmo(String algoritmo) {
+        this.algoritmo = algoritmo;
+    }
+
+    public String getEscenario() {
+        return escenario;
+    }
+
+    public void setEscenario(String escenario) {
+        this.escenario = escenario;
+    }
+
+    public int getSample() {
+        return sample;
+    }
+
+    public void setSample(int sample) {
+        this.sample = sample;
+    }
+
+    public double getTiempoNano() {
+        return tiempoNano;
+    }
+
+    public void setTiempoNano(double tiempoNano) {
+        this.tiempoNano = tiempoNano;
+    }
+
+    public double getTiempoMilis() {
+        return tiempoMilis;
+    }
+
+    public void setTiempoMilis(double tiempoMilis) {
+        this.tiempoMilis = tiempoMilis;
+    }
+
+    @Override
+    public String toString() {
+        return "Resultado; algoritmo=" + algoritmo + ", escenario=" + escenario + ", sample=" + sample + ", tiempoNano="
+                + tiempoNano;
+    }
+    
+}
